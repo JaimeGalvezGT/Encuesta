159abca36ba04fc1409069a05d17d6ff1768ab11
JaimeGalvezGT <50521876+JaimeGalvezGT@users.noreply.github.com>
2024-09-30 17:53:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function submitForm(event) {
         data[key] = value;
     });
 
-    fetch('https://script.google.com/macros/s/AKfycbz59k0qw9zUPb2T_eCTcCL-rnUokhWZwIkLjb49hHTyzOWkT7ZSJjF7T-N5Zwi8RNm1/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxiCn5759gyOgiuCA8ZdevPnF8X-3Y2KehCPTs_zdJ2YeqtIyUKJGw_lgQFVL5XyG3u/exec', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
